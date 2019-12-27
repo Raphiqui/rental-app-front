2956a3f583feb459c7f593e35d255501d4f2d037
Norsse <raphael.pastre@gmail.com>
2019-12-27 09:02:02 +0100

    Creating routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Nav from './components/Nav.js';
+import About from './components/About.js';
+import Home from './components/Home/Home.js';
+import Login from "./components/Login/Login.js";
+import Rentals from "./components/Rentals/Rentals.js";
+import RentalDetail from './components/Rentals/Rental/Rental';
+import Connected from "./components/Login/Connected";
+import 'antd/dist/antd.css';
+import SignIn from "./components/SignIn";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Nav/>
+                <Switch>
+                    <Route path="/" exact component={Home}/>
+                    <Route path="/about" component={About}/>
+                    <Route path="/rentals" exact component={Rentals}/>
+                    <Route path="/rentals/:id" component={RentalDetail}/>
+                    <Route path="/login" component={Login}/>
+                    <Route path="/signin" component={SignIn}/>
+                    <Route path="/users/:id" component={Connected}/>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;

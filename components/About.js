97cc35a20d4b1de08da330a2f63ec5e09a5bb857
Norsse <raphael.pastre@gmail.com>
2019-12-27 08:44:08 +0100

    Creating About/Footer/Nav/SignIn components

---
+++ components/About.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import { Container, Divider, Segment } from "semantic-ui-react";
+import { Empty } from 'antd';
+
+export default class About extends Component {
+
+    render() {
+        return (
+            <div className="About">
+                <Segment style={{padding: '8em 0em'}} vertical>
+                    <Container text>
+                        <Divider
+                            as='h4'
+                            className='header'
+                            horizontal
+                            style={{margin: '3em 0em', textTransform: 'uppercase'}}
+                        >
+                            <p>Address</p>
+                        </Divider>
+                        <p style={{fontSize: '1.33em'}}>
+                            21 Rue de la Boustifaille
+                            <br/>
+                            75000 Paris
+                        </p>
+                        <Divider
+                            as='h4'
+                            className='header'
+                            horizontal
+                            style={{margin: '3em 0em', textTransform: 'uppercase'}}
+                        >
+                            <p>Phone number</p>
+                        </Divider>
+                        <p style={{fontSize: '1.33em'}}>
+                            +33 01 02 03 04 05
+                        </p>
+                        <Divider
+                            as='h4'
+                            className='header'
+                            horizontal
+                            style={{margin: '3em 0em', textTransform: 'uppercase'}}
+                        >
+                            <p>Location</p>
+                        </Divider>
+                        <Empty/>
+                    </Container>
+                </Segment>
+            </div>
+        );
+    }
+}
---
+++ components/Nav.js
@@ -0,0 +1,147 @@
+import React, {Component} from "react";
+import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Button, Container, Icon, Menu, Responsive, Segment, Sidebar, Visibility, } from 'semantic-ui-react';
+
+const getWidth = () => {
+    const isSSR = typeof window === 'undefined';
+
+    return isSSR ? Responsive.onlyTablet.minWidth : window.innerWidth
+};
+
+class DesktopContainer extends Component {
+    state = {};
+
+    hideFixedMenu = () => this.setState({ fixed: false });
+    showFixedMenu = () => this.setState({ fixed: true });
+
+    render() {
+        const { children } = this.props;
+        const { fixed } = this.state;
+
+        return (
+            <Responsive getWidth={getWidth} minWidth={Responsive.onlyTablet.minWidth}>
+                <Visibility
+                    once={false}
+                    onBottomPassed={this.showFixedMenu}
+                    onBottomPassedReverse={this.hideFixedMenu}
+                >
+                    <Segment
+                        inverted
+                        textAlign='center'
+                        style={{ minHeight: 80, padding: '1em 0em' }}
+                        vertical
+                    >
+                        <Menu
+                            fixed={fixed ? 'top' : null}
+                            inverted={!fixed}
+                            pointing={!fixed}
+                            secondary={!fixed}
+                            size='large'
+                        >
+                            <Container>
+                                <Menu.Item as={ NavLink } exact to="/">
+                                    Home
+                                </Menu.Item>
+                                <Menu.Item as={ NavLink } to="/rentals">Rentals</Menu.Item>
+                                <Menu.Item position="right">
+                                    <Button as={ NavLink } to="/login" inverted={!fixed}>
+                                        Log in
+                                    </Button>
+                                    <Button as={ NavLink } to="/signin" inverted={!fixed} primary={fixed} style={{ marginLeft: '0.5em' }}>
+                                        Sign Up
+                                    </Button>
+                                </Menu.Item>
+                            </Container>
+                        </Menu>
+                    </Segment>
+                </Visibility>
+
+                {children}
+            </Responsive>
+        )
+    }
+}
+
+DesktopContainer.propTypes = {
+    children: PropTypes.node,
+};
+
+class MobileContainer extends Component {
+    state = {};
+
+    handleSidebarHide = () => this.setState({ sidebarOpened: false });
+
+    handleToggle = () => this.setState({ sidebarOpened: true });
+
+    render() {
+        const { children } = this.props;
+        const { sidebarOpened, fixed } = this.state;
+
+        return (
+            <Responsive
+                as={Sidebar.Pushable}
+                getWidth={getWidth}
+                maxWidth={Responsive.onlyMobile.maxWidth}
+            >
+                <Sidebar
+                    as={Menu}
+                    animation='push'
+                    inverted
+                    onHide={this.handleSidebarHide}
+                    vertical
+                    visible={sidebarOpened}
+                >
+                    <Menu.Item as={NavLink} to="/">Home</Menu.Item>
+                    <Menu.Item as={ NavLink } to="/rentals">Rentals</Menu.Item>
+                    <Menu.Item as={NavLink} to="/login">Log in</Menu.Item>
+                    <Menu.Item as={ NavLink } to="/signin">Sign Up</Menu.Item>
+                </Sidebar>
+
+                <Sidebar.Pusher dimmed={sidebarOpened}>
+                    <Segment
+                        inverted
+                        textAlign='center'
+                        style={{ minHeight: 350, padding: '1em 0em' }}
+                        vertical
+                    >
+                        <Container>
+                            <Menu inverted pointing secondary size='large'>
+                                <Menu.Item onClick={this.handleToggle}>
+                                    <Icon name='sidebar' />
+                                </Menu.Item>
+                                <Menu.Item position='right'>
+                                    <Button as={ NavLink } to="/login" inverted={!fixed}>
+                                        Log in
+                                    </Button>
+                                    <Button as={ NavLink } to="/signin" inverted={!fixed} primary={fixed} style={{ marginLeft: '0.5em' }}>
+                                        Sign Up
+                                    </Button>
+                                </Menu.Item>
+                            </Menu>
+                        </Container>
+                    </Segment>
+
+                    {children}
+                </Sidebar.Pusher>
+            </Responsive>
+        )
+    }
+}
+
+MobileContainer.propTypes = {
+    children: PropTypes.node,
+};
+
+const ResponsiveContainer = ({ children }) => (
+    <div>
+        <DesktopContainer>{children}</DesktopContainer>
+        <MobileContainer>{children}</MobileContainer>
+    </div>
+);
+
+ResponsiveContainer.propTypes = {
+    children: PropTypes.node,
+};
+
+export default ResponsiveContainer

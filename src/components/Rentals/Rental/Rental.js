0a3d43e692f8141c00d211408c7b949cd64373c3
Norsse <raphael.pastre@gmail.com>
2020-01-11 09:13:49 +0100

    Up

--- src/components/Rentals/Rental/Rental.js
+++ src/components/Rentals/Rental/Rental.js
@@ -1,11 +1,12 @@
 import React, {Component, } from "react";
 import {Grid, Segment, Container, Header, Loader,  } from 'semantic-ui-react';
-import { Empty } from 'antd';
+import {DatePicker, Empty} from 'antd';
 import _ from 'lodash';
 import rentalApi from "../../../api/rentalApi.js";
 import ImageCarousel from "./RentalCarousel/ImageCarousel";
 import "pure-react-carousel/dist/react-carousel.es.css";
 import RentalFacilities from "./RentalFacilities";
+import moment from 'moment';
 import Nav from "../../Nav";
 
 export default class Rental extends Component{
@@ -33,48 +34,49 @@ export default class Rental extends Component{
         }
     };
 
-    // disabledDate = (current) => {
-    //     const dates = [
-    //         {
-    //             from: "2020-01-10",
-    //             to: "2020-01-15"
-    //         },
-    //         {
-    //             from: "2020-02-10",
-    //             to: "2020-02-15"
-    //         },
-    //         {
-    //             from: "2020-03-10",
-    //             to: "2020-03-15"
-    //         },
-    //         {
-    //             from: "2020-04-05",
-    //             to: "2020-04-20"
-    //         },
-    //     ];
-    //
-    //     let allDates = [];
-    //
-    //     _.map(dates, date => {
-    //         const currDate = moment(date.from);
-    //         const lastDate = moment(date.to);
-    //         allDates.push(currDate.format('YYYY-MM-DD'));
-    //         while(currDate.add(1, 'days').diff(lastDate) < 0) {
-    //             allDates.push(currDate.clone().format('YYYY-MM-DD'));
-    //         }
-    //         allDates.push(lastDate.format('YYYY-MM-DD'));
-    //     });
-    //
-    //     if (current < moment().endOf('day')){
-    //         return true
-    //     }else{
-    //         let index = allDates.findIndex(date => date === moment(current).format('YYYY-MM-DD'));
-    //         return index !== -1 && true
-    //     }
-    // };
+    disabledDate = (current) => {
+        const dates = [
+            {
+                from: "2020-01-10",
+                to: "2020-01-15"
+            },
+            {
+                from: "2020-02-10",
+                to: "2020-02-15"
+            },
+            {
+                from: "2020-03-10",
+                to: "2020-03-15"
+            },
+            {
+                from: "2020-04-05",
+                to: "2020-04-20"
+            },
+        ];
+
+        let allDates = [];
+
+        _.map(dates, date => {
+            const currDate = moment(date.from);
+            const lastDate = moment(date.to);
+            allDates.push(currDate.format('YYYY-MM-DD'));
+            while(currDate.add(1, 'days').diff(lastDate) < 0) {
+                allDates.push(currDate.clone().format('YYYY-MM-DD'));
+            }
+            allDates.push(lastDate.format('YYYY-MM-DD'));
+        });
+
+        if (current < moment().endOf('day')){
+            return true
+        }else{
+            let index = allDates.findIndex(date => date === moment(current).format('YYYY-MM-DD'));
+            return index !== -1 && true
+        }
+    };
 
     render() {
         const { rental, pictsUrl } = this.state;
+        const { RangePicker } = DatePicker;
 
         let style, message;
         if(!_.isNil(rental)){
@@ -132,6 +134,13 @@ export default class Rental extends Component{
                                         <Header as='h3' style={{fontSize: '2em'}}>
                                             In progress <Loader active inline />
                                         </Header>
+                                        <RangePicker
+                                            style={{marginBottom: '10px'}}
+                                            // onChange={onChangeDate}
+                                            disabledDate={this.disabledDate}
+                                            size="large"
+                                            allowClear={false}
+                                        />
                                     </Grid.Column>
                                 </Grid.Row>
                             </Grid>
